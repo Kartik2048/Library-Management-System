c7542bd1fad36afe6dba6154409943679c774996
Kartik <kartikjuneja2626@gmail.com>
2026-02-18 11:28:39 +0530

    implemented @RequestParam

--- LibraryManagementSystem/src/main/java/com/project/LibraryManagementSystem/controller/LibraryController.java
+++ LibraryManagementSystem/src/main/java/com/project/LibraryManagementSystem/controller/LibraryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.LibraryManagementSystem.entity.Book;
@@ -39,13 +40,13 @@ public class LibraryController {
         return ResponseEntity.ok(libraryService.addMember(member));
     }
 
-    @PostMapping("/borrow/{bookId}/{memberId}")
-    public ResponseEntity<Borrowing> borrowBook(@PathVariable Long bookId, @PathVariable Long memberId) {
+    @PostMapping("/borrow") // requestparam to be used as /borrow?bookId=1&memberId=1
+    public ResponseEntity<Borrowing> borrowBook(@RequestParam(name="bookId", required = true) Long bookId, @RequestParam(name="memberId") Long memberId) {
         return ResponseEntity.ok(libraryService.borrowBook(bookId, memberId));
     }
 
-    @PostMapping("/return/{borrowingId}")
-    public ResponseEntity<Book> returnBook(@PathVariable Long borrowingId) {
+    @PostMapping("/return") // requestparam to be used as /return?borrowingId=1
+    public ResponseEntity<Book> returnBook(@RequestParam(name="borrowingId", required = true) Long borrowingId) {
         return ResponseEntity.ok(libraryService.returnBook(borrowingId));
     }
 }
